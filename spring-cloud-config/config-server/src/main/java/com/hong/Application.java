fe28c433344afb55b6c3616ff3079352a3b1e466
hong <1043518275@qq.com>
2017-06-07 17:30:02 +0800

    配置中心使用git(github)存放配置文件示例

--- spring-cloud-config/config-server/src/main/java/com/hong/Application.java
+++ spring-cloud-config/config-server/src/main/java/com/hong/Application.java
@@ -2,12 +2,14 @@ package com.hong;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.config.server.EnableConfigServer;
 
 /**
  * 配置中心启动类.
  * Created by hong on 2017/6/7.
  */
+@EnableDiscoveryClient
 /* 启动类添加@EnableConfigServer，激活对配置中心的支持 */
 @EnableConfigServer
 @SpringBootApplication
