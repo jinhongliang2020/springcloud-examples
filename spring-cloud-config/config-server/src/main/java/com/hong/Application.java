e8184c030fc2819590d8cecaf95496c067847fca
hong <1043518275@qq.com>
2017-06-08 17:08:19 +0800

    配置中心Client 获取server端的配置信息.

--- spring-cloud-config/config-server/src/main/java/com/hong/Application.java
+++ spring-cloud-config/config-server/src/main/java/com/hong/Application.java
@@ -2,16 +2,12 @@ package com.hong;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.config.server.EnableConfigServer;
 
 /**
  * 配置中心启动类.
  * Created by hong on 2017/6/7.
  */
-/** 启用服务注册与发现 **/
-@EnableDiscoveryClient
-/** 激活对配置中心的支持 **/
 @EnableConfigServer
 @SpringBootApplication
 public class Application {
